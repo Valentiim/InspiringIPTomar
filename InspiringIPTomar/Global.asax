9beb155e8456e4ccb79c57775a6121807f23f74a
Valentim <valentim.arrua@hotmail.com>
2017-03-25 01:04:52 +0000

    Aplicação Web

---
+++ InspiringIPTomar/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="InspiringIPTomar.MvcApplication" Language="C#" %>
---
+++ InspiringIPTomar/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(InspiringIPTomar.Startup))]
+namespace InspiringIPTomar
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
